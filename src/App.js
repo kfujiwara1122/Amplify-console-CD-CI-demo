03e49e252f04e8d6dfdd05bd1a341ec51d95c8aa
kfujiwara1122 <kazu.joker94@gmail.com>
2020-06-21 16:34:21 +0900

    test

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Hello, World. This change is automatically deployed.
         </p>
         <a
           className="App-link"
